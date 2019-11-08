0473966b2280d6c6d0d045f01d7d71eeebf5da83
Mian Huang <47206038+kindht@users.noreply.github.com>
2019-11-08 15:40:57 +0800

    Submit

--- src/tools.cpp
+++ src/tools.cpp
@@ -74,6 +74,6 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   Hj << px/c2,  py/c2,   0, 0,
         -py/c1, px/c1 ,  0, 0,
         py*(vx*py-vy*px)/c3, px*(vy*px-vx*py)/c3 , px/c2 , py/c2 ;
-
+  
   return Hj;
 }
